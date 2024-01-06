dbbe5c14b4d37407a7f9fa28fc6f9077ad83b5cc
Saurabh Pathak <SPathak@eagle.org>
2024-01-06 21:10:09 +0530

    Added Schema and resolvers

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+import {typeDefs} from './schema.js'
+import db from './_db.js'
+
+const resolvers = {
+    Query: {
+        games(){
+            return db.games
+        },
+        reviews(){
+            return db.reviews
+        },
+        authors(){
+            return db.authors
+        }
+    }
+};
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = ApolloServer({
-    // typeDefs,
-    // resolvers,
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
 });
 
 const {url} = await startStandaloneServer(server,{
